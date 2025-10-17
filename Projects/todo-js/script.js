a72ca203099927cab712895501cb0eff007eed2d
Letsmakecake <amaanshahid8267@gmail.com>
2025-10-18 01:26:26 +0530

    practice

--- Projects/todo-js/script.js
+++ Projects/todo-js/script.js
@@ -1,47 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const todoinput = document.getElementById("todo-input");
-  const addtask = document.getElementById("add-task-btn");
-  const todolist = document.getElementById("todo-list");
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.forEach((task) => rendertasks(task));
-  addtask.addEventListener("click", () => {
-    const blah = todoinput.value.trim();
-    if (blah === "") {
-      return;
-    }
-    const newtask = {
-      id: Date.now(),
-      completed: false,
-      text: blah,
-    };
-    tasks.push(newtask);
-    savetasks();
-    rendertasks(newtask);
-    todoinput.value = "";
-    console.log(tasks);
-  });
-  function rendertasks(task) {
-    const li = document.createElement("li");
-    li.setAttribute("data-id", task.id);
-    if (task.completed) li.classList.add("completed");
-    li.innerHTML = `
-    <span>${task.text}</span>
-    <button> Delete </button>`;
-    li.addEventListener("click", (e) => {
-      if (e.target.tagName == "BUTTON") return;
-      task.completed = !task.completed;
-      li.classList.toggle("completed");
-      savetasks();
-    });
-    li.querySelector("button").addEventListener("click", (e) => {
-      e.stopPropagation();
-      tasks = tasks.filter((t) => t.id !== task.id);
-      li.remove();
-      savetasks();
-    });
-    todolist.appendChild(li);
+document.addEventListener("DOMContentLoaded",() => {
+  const TodoInput = document.getElementById('todo-input')
+const AddTaskButton = document.getElementById('add-task-btn')
+const TodoList = document.getElementById('todo-list')
+
+let Tasks = JSON.parse(localStorage.getItem('Tasks')) || [];
+
+Tasks.forEach((Task) => renderTask(Task));
+
+AddTaskButton.addEventListener('click', () => {
+  const Tasktext = TodoInput.value.trim();
+  if(Tasktext ==="") return
+  const NewTask = {
+    id: Date.now(),
+    text: Tasktext,
+    completed: false
   }
-  function savetasks() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }
-});
+  Tasks.push(NewTask);
+  saveTask();
+  renderTask(NewTask)
+  TodoInput.value = "";
+  console.log(Tasks);
+
+})
+function saveTask() {
+  localStorage.setItem('Tasks', JSON.stringify(Tasks))
+}
+function renderTask(Task) {
+  const li = document.createElement('li')
+  li.setAttribute('data-id',Task.id)
+  li.innerHTML=`
+  <span>${Task.text}</span>
+  <button>Delete</button>`;
+  li.addEventListener('click',(e) => {
+    if(e.target.tagName === 'BUTTON') return;
+    Task.completed = !Task.completed;
+    li.classList.toggle('completed')
+    saveTask()
+  })
+  li.querySelector('button').addEventListener('click',(e) => {
+    e.stopPropagation()
+    Tasks = Tasks.filter((t) => t.id !==Task.id);
+    li.remove();
+    saveTask();
+  })
+  TodoList.appendChild(li);
+}
+})
